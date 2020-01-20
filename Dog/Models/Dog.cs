c8513415f3b83bfc16b8c7b00f88f131441eafdb
Robert Shields <roberthshields93@yahoo.com>
2020-01-20 14:31:49 -0800

    Create Dog class

---
+++ Dog/Models/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dog.Models
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Breed { get; set; }
+    }
+}
