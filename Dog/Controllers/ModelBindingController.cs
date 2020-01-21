49aa9fe00320067d14020634747e3e92efba847d
Robert Shields <roberthshields93@yahoo.com>
2020-01-20 16:32:51 -0800

    Finished Model Binding for Dog

---
+++ Dog/Controllers/ModelBindingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogObject.Controllers
+{
+    public class ModelBindingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Dog1 d)
+        {
+            if (ModelState.IsValid) //Ensures model is in valid state
+            {
+                //DogDB.Add(d);
+                return RedirectToAction("Index", "Home");
+            }
+            else // Validation problems occurred
+            {
+                return View();
+            }
+            
+        }
+    }
+}
--- Dog/Models/Dog1.cs
+++ Dog/Models/Dog1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,14 @@ namespace Dog.Models
 {
     public class Dog1
     {
+        [Required(ErrorMessage = "Dog's Name required")]
         public string Name { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Dog's Breed required")]
         public string Breed { get; set; }
     }
 }
